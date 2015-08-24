ade881703eacbb1eae5a8806c83378418e926df2
thodep <thodep@yahoo.com>
2015-08-24 16:34:04 -0400

    Got the picture to show , need to add label and text field

---
+++ Kindling/LayerOfView.swift
@@ -0,0 +1,33 @@
+//
+//  LayerOfView.swift
+//  Kindling
+//
+//  Created by tho dang on 2015-08-24.
+//  Copyright (c) 2015 Tho Dang. All rights reserved.
+//
+
+import UIKit
+
+class LayerOfView: UIImageView {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.CGColor
+        }
+        
+    }
+
+    
+
+}
---
+++ Kindling/RootPresentationViewController.swift
@@ -0,0 +1,45 @@
+//
+//  RootPresentationViewController.swift
+//  Kindling
+//
+//  Created by tho dang on 2015-08-24.
+//  Copyright (c) 2015 Tho Dang. All rights reserved.
+//
+
+import UIKit
+
+class RootPresentationViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+      
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        imageView.layer.cornerRadius = (imageView.layer.bounds.size.width + imageView.layer.bounds.height) / 4
+        imageView.clipsToBounds = true
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
