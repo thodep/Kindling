2f605321e0f4d7479c3306deabdca4aca2e89836
thodep <thodep@yahoo.com>
2015-08-27 17:27:37 -0400

    Fixed Stage 2 - Got the switch case to work

--- Kindling/CardViewController.swift
+++ Kindling/CardViewController.swift
@@ -10,64 +10,56 @@ import UIKit
 
 class CardViewController: UIViewController {
 
-   // @IBOutlet weak var cardImageView: LayerOfView!
     
     @IBOutlet weak var cardImageView: LayerOfView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        cardImageView.layer.cornerRadius = (cardImageView.layer.bounds.size.width + cardImageView.layer.bounds.height) / 4
+        
+        cardImageView.layer.cornerRadius = (cardImageView.layer.bounds.size.width +
+                                             cardImageView.layer.bounds.height) / 4
         cardImageView.clipsToBounds = true
-        // Do any additional setup after loading the view.
     
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
       
     }
     
-    @IBAction func didPan(sender: UIPanGestureRecognizer) {
+
     
-        let translation = sender.translationInView(self.view)
-        if let view = sender.view {
-            view.center = CGPoint(x:view.center.x + translation.x,
-                y:view.center.y + translation.y)
-        }
-        sender.setTranslation(CGPointZero, inView: self.view)
-        print("Let ")
-        
-        // Do we need these codes
-        var transV = sender.translationInView(view)
-        print("All is well. X: \(transV)")
-        
-        // Set UIGesture States
-        if sender.state == UIGestureRecognizerState.Began {
-        print("began")
-        }
-        else if sender.state == UIGestureRecognizerState.Ended {
-              view.center = CGPoint(x: self.view.bounds.width / 2, y: self.view.bounds.height / 2)
-        print("ended")
-          
-        } else {
-            print(" user stopped panning")
-        }
-        
-        
-        // interesting case of using Switch
-//        switch sender.state {
-//        case UIGestureRecognizerState.Began:
-//            print("start")
-//        case UIGestureRecognizerState.Ended:
-//            print("ended")
-//        default:
-//            print("nothing")
+//        let translation = sender.translationInView(self.view)
+//        if let view = sender.view {
+//        view.center = CGPoint(x:view.center.x + translation.x,
+//                              y:view.center.y + translation.y)
 //        }
-    }
+//        
+//        sender.setTranslation(CGPointZero, inView: self.view)
+//        print("Let ")
+//        
+//        // Do we need these codes??
+//        var transV = sender.translationInView(view)
+//        print("All is well. X: \(transV)")
+//        
+//        // Set UIGesture States
+//        if sender.state == UIGestureRecognizerState.Began {
+//        print("began")
+//        }
+//        else if sender.state == UIGestureRecognizerState.Ended {
+//        // Move the Card View back to the center when user stop panning
+//        view.center = CGPoint(x: self.view.bounds.width / 2,
+//                              y: self.view.bounds.height / 2)
+//        print("ended")
+//          
+//        } else {
+//        print(" user stopped panning")
+//        }
+
+           
     
- 
 }
--- Kindling/RootPresentationViewController.swift
+++ Kindling/RootPresentationViewController.swift
@@ -12,12 +12,23 @@ class RootPresentationViewController: UIViewController {
     
     @IBOutlet weak var horizontalContraint: NSLayoutConstraint!
     @IBOutlet weak var cardContainer: UIView!
+    @IBOutlet weak var cardTopLayoutConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
       
+        super.viewDidLoad()
+        
+        //Adding Selection States
+        enum SelectionState {
+        
+        case NoSelection
+        case LikeSelection
+        case DislikeSelection
+        
+        }
+        var currentState = SelectionState.NoSelection
+        print("\(currentState )")
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,6 +46,35 @@ class RootPresentationViewController: UIViewController {
     }
     */
 
+    @IBAction func handlePan(sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case UIGestureRecognizerState.Began:
+            println("began")
+          
+        case UIGestureRecognizerState.Ended:
+            println("eND")
+            horizontalContraint.constant = 0.0
+            cardContainer.layoutIfNeeded()
+            
+        case UIGestureRecognizerState.Changed:
+            let translation = sender.translationInView(self.cardContainer)
+//            var curveOffset:CGFloat = cos(translation.x / (0.5 * self.view.bounds.width))
+//            println("The current curve offest = \(curveOffset)")
+//            cardTopLayoutConstraint.constant = (-curveOffset * 30)
+            horizontalContraint.constant =  -translation.x
+            cardContainer.layoutIfNeeded()
+
+        case UIGestureRecognizerState.Failed:
+            println("OPPS ERROR")
+            
+        default:
+            print("Default callback triggered")
+            
+
+            
+        }
+
+    }
     
 
 }
